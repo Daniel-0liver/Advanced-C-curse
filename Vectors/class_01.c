cca72f3f9d11164b84e77182e0c4adcc541bea82
Daniel Oliveira <dtoliverbr@gmail.com>
2021-10-27 04:13:10 +0100

    Vectors_aritmetic.c

--- Vectors/class_01.c
+++ Vectors/class_01.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int v[5] = {0, 1, 2, 3, 4,};
+    int v[5] = {0, 1, 2, 3, 4};
 
     for (int i = 0; i < 5; i++)
     {
         printf("&v[%d] = %p, v[%d] = %d\n", i, &v[i], i, v[i]);
     }
 
-    printf("&v[6] = %p, v[6] = %d\n\n", &v[6], v[6]);
-    printf("&v[7] = %p, v[7] = %d\n\n", &v[7], v[7]);
+    //Show that I can call one memory position that I don't allocate, and don't return error.
+    printf("&v[6] = %p, v[6] = %d\n\n", &v[5], v[5]);
+    printf("&v[7] = %p, v[7] = %d\n\n", &v[6], v[6]);
     
     return 0;
-    
 }
